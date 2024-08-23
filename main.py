de5c62d791c3289a7a05ca82ad43660ef3d48376
PharmacyDoc2018 <pharmacydoc2018@gmail.com>
2024-08-23 14:02:53 -0400

    Added circleshape.py and player.py. Main.py draws triangle player in center of the screen

--- main.py
+++ main.py
@@ -3,11 +3,16 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -15,10 +20,14 @@ def main():
                 return
             
         screen.fill((0, 0, 0))
+        player.draw(screen)
+
         pygame.display.flip()
-    print("Starting asteroids!")
-    print("Screen width:",SCREEN_WIDTH)
-    print("Screen height:",SCREEN_HEIGHT)
+        dt = clock.tick(60) / 1000
+    
+    #print("Starting asteroids!")
+    #print("Screen width:",SCREEN_WIDTH)
+    #print("Screen height:",SCREEN_HEIGHT)
 
 if __name__ == "__main__":
     main()
